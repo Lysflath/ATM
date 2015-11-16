fa7e7c3636c33f23aef874eb21cbb19b24e4004a
Lysflath <syncoreee@gmail.com>
2015-11-16 02:42:02 +0200

    Update 1.0

---
+++ ATM/ATM/ChooseLanguageWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ATM
+{
+    /// <summary>
+    /// Логика взаимодействия для ChooseLanguageWindow.xaml
+    /// </summary>
+    public partial class ChooseLanguageWindow : Window
+    {
+        public ChooseLanguageWindow()
+        {
+            InitializeComponent();
+            System.Windows.Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+
+    }
+}
